f011b623c892144e17e6aa5b3b486da85a1745e6
amarufd <amarud@gmail.com>
2019-04-12 20:39:11 -0300

    se agregaron funciones

--- miPrimerScript.R
+++ miPrimerScript.R
@@ -1,3 +1,35 @@
+# rm(list = ls())
 # instalar paquete ggplot2
 # install.packages('ggplot2')
 library(ggplot2)
+
+
+variableUno <- 1
+variableDos <- 5
+
+variableUno <- FALSE 
+
+variableDos <- TRUE
+
+variableTres <- 'fui a la esquina y volvi'
+
+variableTres <- 1.6
+
+rm(variableDos)
+
+variableTres <- variableUno != variableDos
+
+# OR logico
+variableCuatro <- !((variableUno < variableDos) && (variableUno > variableTres))
+
+# AND logico
+FALSE && TRUE
+
+
+## ===========================
+variableUno <- variableUno^2 + 1
+
+
+funcionUno()
+
+
